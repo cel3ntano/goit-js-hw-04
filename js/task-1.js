f631477a6910458201d684a239a10edea76ea067
cel3ntato <adriano2024celentano@gmail.com>
2024-04-02 21:11:39 +0200

    hw4

---
+++ js/task-1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function isEnoughCapacity(products, containerSize) {
+  let productsTotal = 0;
+  for (const quantity in products) {
+    productsTotal += products[quantity];
+  }
+  return productsTotal <= containerSize;
+}
+
+// function isEnoughCapacity(products, containerSize) {
+//   let productsTotal = 0;
+//   for (const quantity of Object.values(products)) {
+//     productsTotal += quantity;
+//   }
+//   return productsTotal <= containerSize;
+// }
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
